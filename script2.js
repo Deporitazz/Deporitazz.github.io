8b5409fc83f7e503de782702dbdf20bcff1179bf
Deporitazz <iswavy412@gmail>
2022-11-15 14:04:33 -0600

    changes

---
+++ script2.js
@@ -0,0 +1,10 @@
+let showNav=false;
+
+function navToggle() {
+    showNav=!showNav;
+    if (showNav) {
+        document.getElementsByTagName('nav').item(0).classList.add('show');
+    } else {
+        document.getElementsByTagName('nav').item(0).classList.remove('show');
+    }
+}
