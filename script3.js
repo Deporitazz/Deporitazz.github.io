6d60a72a954a04bbd511824aa62fd94c046223fd
Deporitazz <114692872+Deporitazz@users.noreply.github.com>
2022-12-14 17:33:15 -0600

    YIPPEEEE

---
+++ script3.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 20,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop: true,
+ });
