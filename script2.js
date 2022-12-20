77560b123dd2bb8432b450b70314261f1ecaa5a6
Deporitazz <114692872+Deporitazz@users.noreply.github.com>
2022-12-19 23:15:05 -0600

    ??

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 var egg = new Egg();
 egg.addCode("up,up,down,down", function() {
-    alert('done')
+    alert('Congrats you have access to the new site design!')
   }).addHook(function(){
     console.log("Hook called for: " + this.activeEgg.keys);
     console.log(this.activeEgg.metadata);
