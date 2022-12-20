76c1a823eab36f8740a417d2b438fa2f02a69446
Deporitazz <114692872+Deporitazz@users.noreply.github.com>
2022-12-19 23:18:52 -0600

    ugv

--- script2.js
+++
@@ -1,7 +0,0 @@
-var egg = new Egg();
-egg.addCode("up,up,down,down", function() {
-    alert('Congrats you have access to the new site design!')
-  }).addHook(function(){
-    console.log("Hook called for: " + this.activeEgg.keys);
-    console.log(this.activeEgg.metadata);
-  }).listen();
